a8bbcde16c24c18045ff80c7c1babb5b109cd4a1
Tobias <juantobiasdl7@gmail.com>
2023-11-20 02:19:06 -0600

    I added the method to make a transcription from server side using node and openai.

--- server/controllers/chatControllers.js
+++ server/controllers/chatControllers.js
@@ -1,12 +1,27 @@
 const openAIHelper = require('../helpers/openAIHelper');
+const fs = require('fs');
 // const audioHelper = require('../helpers/audioHelper');
 
 exports.processAudio = async (req, res) => {
     try {
-        // This function would handle the uploaded audio file,
+        // This function would handle the uploaded audio file and saved it to the server
+            // req.file contains the audio file
+        const audioBuffer = req.file.buffer;
+        const audioPath = 'C:/Users/juant/Desktop/Projects/Talk to ChatGPT Web App/server/audio_uploads/audio.mp3';
+
+        // Save the buffer to a file
+        fs.writeFile(audioPath, audioBuffer, (err) => {
+            if (err) {
+                console.log('Error saving audio:', err);
+                console.error('Error saving audio:', err);
+            }
+            console.log('Audio saved successfully');
+        });
+
+        
         // send it to OpenAI for transcription, get the chat response,
         // generate the audio from the text response, and return it to the client.
-        const transcription = await openAIHelper.transcribeAudio(req.file); // You'll need to set up file handling middleware like 'multer'.
+        const transcription = await openAIHelper.transcribeAudio(audioPath, req.file); // You'll need to set up file handling middleware like 'multer'.
         // const response = await openAIHelper.getChatResponse(transcription);
         // const audioResponse = await openAIHelper.textToAudio(response);
         
--- server/helpers/openAIHelper.js
+++ server/helpers/openAIHelper.js
@@ -1,41 +1,76 @@
 const OpenAI = require("openai");
 const { Readable } = require('stream');
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const fetch = require('node-fetch-commonjs');
 
 console.log('API Key:', process.env.OPENAI_API_KEY); // This should print out your actual API key
-
 
 // Configure the OpenAI API client
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-const bufferToStream = (buffer) => {
-    const stream = new Readable();
-    stream.push(buffer);
-    stream.push(null); // Signifies the end of the stream
-    return stream;
-};
-
-exports.transcribeAudio = async (audioBuffer) => {
+exports.transcribeAudio = async (audioPath, reqFile) => {
     // Logic to transcribe audio using OpenAI's API
     // Placeholder for transcription logic
     try {
 
-        const audioStream = bufferToStream(audioBuffer.buffer);
+        let form = new FormData();        
 
-        // Assuming OpenAI provides an endpoint for Whisper
-        const response = await openai.whisper.transcribe({
-            audio: audioStream,
-            model: "whisper-1",
-            // Other necessary options
+        const newfile = fs.createReadStream(audioPath);
+
+        newfile.on('open', () => {
+            console.log('Stream opened:', audioPath);
+        });
+        
+        newfile.on('error', (err) => {
+            console.error('Error with stream:', audioPath, err);
+        });
+        
+        newfile.on('ready', () => {
+            console.log('Stream is ready:', audioPath);
         });
 
-        return response.transcription.text;
-    } catch (error) {
-        console.error('Error in transcribing audio:', error);
-        throw error;
-    }
-};
+        form.append('file', newfile, {
+            filename: reqFile.originalname,
+        });
+        form.append('model', 'whisper-1');
+        
+
+        const res = await fetch("https://api.openai.com/v1/audio/transcriptions", {
+            method: "POST",
+            body: form,
+            headers: {
+                Authorization: "Bearer " + process.env.OPENAI_API_KEY,
+            },
+        });
+        const data = await res.json(); // parses the response body as JSON
+        console.log(data);
+
+        
+        
+        return res
+        } catch (error) {
+            console.log("This is the error.")
+            console.error(error.response ? error.response.data : error.message);
+            throw new Error(error);
+        }
+}
+        // // Assuming OpenAI provides an endpoint for Whisper
+        // const response = await openai.audio.transcriptions.create({
+        //     audio: audioStream,
+        //     model: "whisper-1",
+        //     // Other necessary options
+        // });
+
+        // return response.transcription.text;
+    // } catch (error) {
+    //     console.error('Error in transcribing audio:', error);
+    //     throw error;
+    // }
+//};
 
 // exports.getChatResponse = async (transcription) => {
 //     // Logic to send the transcription to OpenAI and get a chat response
